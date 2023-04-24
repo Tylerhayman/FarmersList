94f8f2caa5831209c4d7568c4beea76f5406bf10
Tylerhayman <128461465+Tylerhayman@users.noreply.github.com>
2023-04-24 11:44:11 +0100

    Add files via upload

---
+++ classes/Cart.js
@@ -0,0 +1,20 @@
+class Cart {
+    constructor(products,total) {
+        this.products = products = [];
+        this.total = 0;
+    }
+
+
+    addProduct() {
+        this.products.push(Product);
+        Product + this.total;
+    }
+
+
+    removeProduct(i) {
+        this.products.pop(Product[i]);
+        this.total - Product[i];
+    }
+}
+
+module.exports = Cart
