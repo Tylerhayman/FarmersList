b4721df9eac18b9ab758efe5a1847ad4c2c69f9b
Tylerhayman <128461465+Tylerhayman@users.noreply.github.com>
2023-04-24 11:42:47 +0100

    Add files via upload

---
+++ Auth.js
@@ -0,0 +1,17 @@
+class Auth {
+    constructor(customers) {
+        this.customers = class{Customer} = [ ]
+    }
+
+
+    register(name,email,shippingAddress) {
+        unshift({Customer}).customers;
+    }
+
+    login(email) {
+        return find({Customer}).email;
+    }
+}
+
+
+module.exports = Auth
